c4bb83d746dad148a445dbdf4b5573fa5774168f
LordAkirast <darkwarish@gmail.com>
2024-01-09 10:24:10 -0300

    correction on tickets-router colocando gettickettypes ao invés de puxar o CEP

--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -2,12 +2,12 @@ import { Router } from 'express';
 import { authenticateToken, validateBody } from '@/middlewares';
 import { getEnrollmentByUser, postCreateOrUpdateEnrollment, getAddressFromCEP } from '@/controllers';
 import { createOrUpdateEnrollmentSchema } from '@/schemas';
-
+import { getTicketTypes } from '@/controllers/tickets-controller';
 const ticketsRouter = Router();
 
 ticketsRouter
-  .all('/*', authenticateToken)
-  .get('/types', getAddressFromCEP) //get tickets types
+  //.all('/*', authenticateToken)
+  .get('/types', getTicketTypes) //get tickets types
   .get('/', getEnrollmentByUser) //get tickets
   .post('/', validateBody(createOrUpdateEnrollmentSchema), postCreateOrUpdateEnrollment); //post tickets
 
